37f08163ed5372837c5c7af5b1a73bd5c044f461
William Galas <williamgalas2@gmail.com>
2022-04-08 19:43:48 +0800

    Update DownloadResume.vue

--- src/components/DownloadResume.vue
+++ src/components/DownloadResume.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="../assets/william galas resume.pdf" download class=" border-b-4  border-gray-900 fixed bg-gray-300 p-4 w-full text-center uppercase font-mono font-bold top-0 right-0 hover:bg-black hover:text-white transition-all">
+    <a href="//github.com/will3452/showcase/raw/main/src/assets/william%20galas%20resume.pdf" download class=" border-b-4  border-gray-900 fixed bg-gray-300 p-4 w-full text-center uppercase font-mono font-bold top-0 right-0 hover:bg-black hover:text-white transition-all">
         Download Willam's Resume
     </a>
 </template>
